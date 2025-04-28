b432a7e8c05a0ff8b46a766313495d17aa062302
ThugSai2000 <125567260+ThugSai2000@users.noreply.github.com>
2025-04-28 10:46:34 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import { Button } from '../components/ui/button';
+
+const Home = () =>
+{
+    return (
+        <>
+            <h2>Welcome to my Home page</h2>
+            <Button variant="link">Click me</Button>
+            <Link to="/about">Go to about page</Link>
+        </>
+    )
+}
+
+export default Home;
